f6f082f7927389f5056288fb817013f48601bd36
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-04-11 21:06:42 +0300

    Blazor Veri Listeleme | Blazor List User Data | Blazor Pages

---
+++ MealOrdering/Client/Pages/PageProcess/UserListProcess.cs
@@ -0,0 +1,25 @@
+﻿using MealOrdering.Shared.DTOs;
+using MealOrdering.Shared.ResponseModels;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace MealOrdering.Client.Pages.Users
+{
+    public partial class UserListProcess : ComponentBase
+    {
+        [Inject]
+        HttpClient HttpClient { get; set; }
+        protected List<UserDTO> UserList { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadListAsync();
+        }
+
+        protected async Task LoadListAsync()
+        {
+            ServiceResponse<List<UserDTO>> serviceResponse = await HttpClient.GetFromJsonAsync<ServiceResponse<List<UserDTO>>>("api/Users/GetUsers");
+            if (serviceResponse.Success)
+                UserList = serviceResponse.Value;
+        }
+    }
+}
