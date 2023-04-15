6b265f47df5c2eaa26721f5230f908ac61992037
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-04-15 21:44:50 +0300

    SaveChanges Updated

--- MealOrdering.Server.Data/Contexts/MealOrderingDbContext.cs
+++ MealOrdering.Server.Data/Contexts/MealOrderingDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using MealOrdering.Server.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Reflection;
 
 namespace MealOrdering.Server.Data.Contexts
 {
     public class MealOrderingDbContext : DbContext
     {
-        public MealOrderingDbContext(DbContextOptions<MealOrderingDbContext> options) : base(options){}
+        public MealOrderingDbContext(DbContextOptions<MealOrderingDbContext> options) : base(options) { }
 
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
@@ -18,6 +19,17 @@ namespace MealOrdering.Server.Data.Contexts
             modelBuilder.HasPostgresExtension("uuid-ossp");
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            IEnumerable<EntityEntry<BaseEntity>> datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (EntityEntry<BaseEntity> data in datas)
+                if (data.State == EntityState.Added)
+                    data.Entity.CreatedDate = DateTime.UtcNow;
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
